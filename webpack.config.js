e4802c25ed231d55c1e394045f144cd99712456f
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-01-31 23:00:17 +0100

    development & production

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
+module.exports =(env) => {
+    const environment = env || 'production';
+    return {
+        mode: environment,
+        entry: './src/index.js',
         output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-       rules: [
-    {
-        test: /\.js$/,
-        loader: "babel-loader"
-    },
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                },
+
     {
         test: /\.css$/,
         use: [
@@ -25,5 +30,6 @@ module.exports = {
              ]
             }
         ]
+     }
     }
 };
